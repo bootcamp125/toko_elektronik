bbac92f3c921248880090fe838d66553d34339fa
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-26 17:54:07 +0700

    222

--- TokoElektronik/src/main/java/com/xsis/training125/model/Penjualan.java
+++ TokoElektronik/src/main/java/com/xsis/training125/model/Penjualan.java
@@ -3,12 +3,18 @@ package com.xsis.training125.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class Penjualan {
 
 	@Id
@@ -16,78 +22,63 @@ public class Penjualan {
 	@Column(name="no_nota")
 	private int noNota;
 	private Date tanggalPenjualan;
-	private Barang idBarang;
+	
 	private int totalBarang;
 	private int totalHarga;
-	private Karyawan idKaryawan;
-	@ManyToOne
-	@JoinColumn(name="id_pelanggan")
-	private Pelanggan idPelanggan;
-	private Diskon diskon;
+
 	
 	public Penjualan(){}
 	
-	public Penjualan(int noNota, Date tanggalPenjualan, Barang idBarang, int totalBarang, int totalHarga,
-			Karyawan idKaryawan, Pelanggan idPelanggan, Diskon diskon) {
+
+
+	public Penjualan(int noNota, Date tanggalPenjualan, int totalBarang, int totalHarga) {
 		super();
 		this.noNota = noNota;
 		this.tanggalPenjualan = tanggalPenjualan;
-		this.idBarang = idBarang;
 		this.totalBarang = totalBarang;
 		this.totalHarga = totalHarga;
-		this.idKaryawan = idKaryawan;
-		this.idPelanggan = idPelanggan;
-		this.diskon = diskon;
 	}
+
+
+
 	public int getNoNota() {
 		return noNota;
 	}
+
+
 	public void setNoNota(int noNota) {
 		this.noNota = noNota;
 	}
+
+
 	public Date getTanggalPenjualan() {
 		return tanggalPenjualan;
 	}
+
+
 	public void setTanggalPenjualan(Date tanggalPenjualan) {
 		this.tanggalPenjualan = tanggalPenjualan;
 	}
-	public Barang getIdBarang() {
-		return idBarang;
-	}
-	public void setIdBarang(Barang idBarang) {
-		this.idBarang = idBarang;
-	}
+
+
 	public int getTotalBarang() {
 		return totalBarang;
 	}
+
+
 	public void setTotalBarang(int totalBarang) {
 		this.totalBarang = totalBarang;
 	}
-	public Karyawan getIdKaryawan() {
-		return idKaryawan;
-	}
-	public void setIdKaryawan(Karyawan idKaryawan) {
-		this.idKaryawan = idKaryawan;
-	}
+
+
 	public int getTotalHarga() {
 		return totalHarga;
 	}
+
+
 	public void setTotalHarga(int totalHarga) {
 		this.totalHarga = totalHarga;
 	}
-	public Pelanggan getIdPelanggan() {
-		return idPelanggan;
-	}
-	public void setIdPelanggan(Pelanggan idPelanggan) {
-		this.idPelanggan = idPelanggan;
-	}
-	public Diskon getDiskon() {
-		return diskon;
-	}
-	public void setDiskon(Diskon diskon) {
-		this.diskon = diskon;
-	}
-	
 	
 	
 }
