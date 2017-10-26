e3f4c5a638e3dbbb4f0b845c3281a65036140341
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-26 11:55:32 +0700

    11

--- TokoElektronik/src/main/java/com/xsis/training125/controller/Address.java
+++ TokoElektronik/src/main/java/com/xsis/training125/controller/Address.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Address {
 
-	private String nama;
+	private String namee;
 	private String postalCode;
 	
 	public String getName() {
