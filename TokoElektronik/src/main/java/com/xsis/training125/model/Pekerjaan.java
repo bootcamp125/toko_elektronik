39c2dc7329004e71f45bd2185e2d808cd71506cc
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-27 16:11:40 +0700

    2

---
+++ TokoElektronik/src/main/java/com/xsis/training125/model/Pekerjaan.java
@@ -0,0 +1,48 @@
+package com.xsis.training125.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+public class Pekerjaan {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_pekerjaan")
+	private int idPekerjaan;
+	private String deskripsi;
+	
+	
+	public Pekerjaan(){}
+	
+	
+	public Pekerjaan(int idPekerjaan, String deskripsi) {
+		super();
+		this.idPekerjaan = idPekerjaan;
+		this.deskripsi = deskripsi;
+	}
+	public int getIdPekerjaan() {
+		return idPekerjaan;
+	}
+	public void setIdPekerjaan(int idPekerjaan) {
+		this.idPekerjaan = idPekerjaan;
+	}
+	public String getDeskripsi() {
+		return deskripsi;
+	}
+	public void setDeskripsi(String deskripsi) {
+		this.deskripsi = deskripsi;
+	}
+	
+	
+	
+}
