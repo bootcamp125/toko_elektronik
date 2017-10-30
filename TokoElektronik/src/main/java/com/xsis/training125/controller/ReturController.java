b168b934d5827bab291e5d43e6b6f72e5ee7bd95
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-30 13:20:56 +0700

    zz

--- TokoElektronik/src/main/java/com/xsis/training125/controller/ReturController.java
+++ TokoElektronik/src/main/java/com/xsis/training125/controller/ReturController.java
@@ -1,5 +1,27 @@
 package com.xsis.training125.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.xsis.training125.model.Retur;
+import com.xsis.training125.service.ReturService;
+
+@Controller
+@RequestMapping("/retur")
 public class ReturController {
 
+	@Autowired
+	ReturService returService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String index(Model model){
+		List<Retur> returs=returService.getAllRetur();
+		model.addAttribute("retur",returs);
+		return "retur";
+	}
 }
--- TokoElektronik/src/main/java/com/xsis/training125/dao/ReturDaoImpl.java
+++ TokoElektronik/src/main/java/com/xsis/training125/dao/ReturDaoImpl.java
@@ -1,5 +1,42 @@
 package com.xsis.training125.dao;
 
-public class ReturDaoImpl {
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xsis.training125.model.Retur;
+
+@Repository
+public class ReturDaoImpl implements ReturDao{
+
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	@Override
+	public void save(Retur retur) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.save(retur);
+		session.flush();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xsis.training125.dao.ReturDao#getAllRetur()
+	 */
+	@Override
+	public List<Retur> getAllRetur() {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		String myHql ="from RETUR";
+		List<Retur> returs = session.createQuery(myHql).list();
+		if(returs.isEmpty()){
+			return null;
+		}
+		return returs;
+	}
 
 }
