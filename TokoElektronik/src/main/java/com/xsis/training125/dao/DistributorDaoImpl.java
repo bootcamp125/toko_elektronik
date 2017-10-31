47d06a5cf8904b3916cc64ca3f48c8643e503b51
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-31 09:03:36 +0700

    distributor

--- TokoElektronik/src/main/java/com/xsis/training125/dao/DistributorDaoImpl.java
+++ TokoElektronik/src/main/java/com/xsis/training125/dao/DistributorDaoImpl.java
@@ -7,8 +7,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.xsis.training125.model.Diskon;
 import com.xsis.training125.model.Distributor;
-import com.xsis.training125.model.Employee;
 
 @Repository
 public class DistributorDaoImpl implements DistributorDao {
@@ -28,28 +28,45 @@ public class DistributorDaoImpl implements DistributorDao {
 	@Override
 	public List<Distributor> getAllDistributor() {
 		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+		
+		// HQL (hibernate query language)
+		String myHql = "from distributor";
+		List<Distributor> distributor = session.createQuery(myHql).list();
+		if(distributor.isEmpty()){
+			return null;
+		}
+		
+		return distributor;
 	}
 
 	
 	@Override
 	public Distributor getDistributorById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+		Distributor distributor = session.get(Distributor.class, id);
+		return distributor;
 	}
 
 
 	@Override
 	public void update(Distributor distributor) {
 		// TODO Auto-generated method stub
-		
+		Session session = sessionFactory.getCurrentSession();
+		session.update(distributor);
+		session.flush();
 	}
 
 	
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
-		
+		Session session = sessionFactory.getCurrentSession();
+		Distributor distributor = new Distributor();
+		distributor.setId(id);	
+		session.delete(distributor);
+		session.flush();
 	}
 
 }
