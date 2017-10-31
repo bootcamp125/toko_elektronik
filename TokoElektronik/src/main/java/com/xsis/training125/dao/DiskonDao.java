4287c17edeb83398b72bdecb1aafb50c6f342b4d
DiahSapitri <diahsapitri@gmail.com>
2017-10-31 11:05:13 +0700

    aaa

--- TokoElektronik/src/main/java/com/xsis/training125/dao/DiskonDao.java
+++ TokoElektronik/src/main/java/com/xsis/training125/dao/DiskonDao.java
@@ -3,23 +3,12 @@ package com.xsis.training125.dao;
 import java.util.List;
 
 import com.xsis.training125.model.Diskon;
-<<<<<<< HEAD
-=======
-import com.xsis.training125.model.Retur;
->>>>>>> branch 'master' of https://github.com/bootcamp125/toko_elektronik.git
 
 public interface DiskonDao {
-
-<<<<<<< HEAD
-	List<Diskon> getAllDiskon();
-
-	void save(Diskon diskon);
-=======
 	
 	void save(Diskon diskon);
 
 	List<Diskon> getAllDiskon();
->>>>>>> branch 'master' of https://github.com/bootcamp125/toko_elektronik.git
 
 	Diskon getDiskonById(int id);
 
