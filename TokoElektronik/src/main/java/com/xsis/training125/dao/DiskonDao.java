f6a126ea538e9899e54e54870dc95b6e2e5ab129
DESKTOPNB-0024\XSIS-NB0034 <bramaers@gmail.com>
2017-10-31 08:43:53 +0700

    diskon

--- TokoElektronik/src/main/java/com/xsis/training125/dao/DiskonDao.java
+++ TokoElektronik/src/main/java/com/xsis/training125/dao/DiskonDao.java
@@ -1,5 +1,21 @@
 package com.xsis.training125.dao;
 
+import java.util.List;
+
+import com.xsis.training125.model.Diskon;
+import com.xsis.training125.model.Retur;
+
 public interface DiskonDao {
+
+	
+	void save(Diskon diskon);
+
+	List<Diskon> getAllDiskon();
+
+	Diskon getDiskonById(int id);
+
+	void update(Diskon diskon);
+
+	void delete(int id);
 
 }
--- TokoElektronik/src/main/java/com/xsis/training125/service/DiskonService.java
+++ TokoElektronik/src/main/java/com/xsis/training125/service/DiskonService.java
@@ -1,5 +1,44 @@
 package com.xsis.training125.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.xsis.training125.dao.DiskonDao;
+import com.xsis.training125.dao.ReturDao;
+import com.xsis.training125.model.Diskon;
+import com.xsis.training125.model.Retur;
+
+@Transactional
+@Service
 public class DiskonService {
 
+	@Autowired
+	DiskonDao diskonDao;
+	
+	public void save(Diskon diskon){
+		diskonDao.save(diskon);
+	}
+	
+	public List<Diskon> getAllDiskon(){
+		return diskonDao.getAllDiskon();
+	}
+	
+	public Diskon getDiskonById(int id){
+		return diskonDao.getDiskonById(id);
+	}
+	
+
+	public void update(Diskon diskon) {
+		// TODO Auto-generated method stub
+		diskonDao.update(diskon);
+	}
+
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		diskonDao.delete(id);
+	}
+	
 }
